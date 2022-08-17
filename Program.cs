8adba05374b5ea457957fae1161edc637a1375de
swalihmp <swalihmp438368@gmail.com>
2022-08-17 13:59:27 +0530

    .net

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current Day is "+ DateTime.Now.ToString("dddd"));
+Console.WriteLine("The current Month is "+ DateTime.Now.ToString("MMMM"));
